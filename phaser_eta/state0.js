c0819cc2d2b447b1a1af112264524cacea3a5d19
meincrab <meincrab@gmail.com>
2018-01-25 23:21:50 +0200

    Tasks until Section 5 25 DONE

---
+++ phaser_eta/state0.js
@@ -0,0 +1,10 @@
+var demo = {};
+demo.state0 = function(){};
+demo.state0.prototype = {
+    preload: function() {},
+    create: function() {
+        game.stage.backgroundColor = '#80ff80';
+        console.log('state0');
+    },
+    update: function() {}
+};
