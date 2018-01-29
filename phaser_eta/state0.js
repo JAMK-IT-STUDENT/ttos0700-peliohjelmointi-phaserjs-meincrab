761579e8cc75fff5535ea97a7eec90c11fe573bd
meincrab <meincrab@gmail.com>
2018-01-29 02:46:24 +0200

    State 0 done

--- phaser_eta/state0.js
+++ phaser_eta/state0.js
@@ -1,15 +1,58 @@
-var demo = {};
+var demo = {} , centerX = 1500 / 2 , centerY = 1000 / 2, doge, speed = 4;
 demo.state0 = function(){};
 demo.state0.prototype = {
-    preload: function() {},
+    preload: function() {
+        game.load.spritesheet('doge', 'assets/sprites/wolf_anim.png', 127, 65)
+        game.load.image('ground', 'assets/sprites/ground1.png')
+    },
     create: function() {
+        game.physics.startSystem(Phaser.Physics.ARCADE);
         game.stage.backgroundColor = '#80ff80';
         console.log('state0');
 
       addChangeStateEventListeners();
+      game.world.setBounds(0,0, 2813, 1000);
+      game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+      var ground = game.add.sprite(0, 0, 'ground');
+      doge = game.add.sprite(centerX, centerY, 'doge');
+      doge.anchor.setTo(0.5, 1.5);   
+      doge.scale.setTo(0.7, 0.7);
+      game.physics.enable(doge);
+      doge.body.collideWorldBounds = true;
+      doge.animations.add('walk', [0, 1, 2,3, 4]);
 
+      game.camera.follow(doge);
+      game.camera.deadzone = new Phaser.Rectangle(centerX - 300, 0, 600, 1000);
     },
-    update: function() {}
+    update: function() {
+        if(game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)){
+            doge.scale.setTo(0.7, 0.7);
+            doge.x += speed;
+            doge.animations.play('walk', 14, true);
+        }
+        else if(game.input.keyboard.isDown(Phaser.Keyboard.LEFT)){
+            doge.scale.setTo(-0.7, 0.7);
+            doge.x -= speed;
+            doge.animations.play('walk', 14, true);
+        }
+        else {
+            doge.animations.stop('walk');
+            doge.frame = 0;
+        }
+        if(game.input.keyboard.isDown(Phaser.Keyboard.DOWN)){
+            doge.y += speed;
+            if (doge.y > 660) {
+                doge.y = 660;
+            }
+        }
+        else if(game.input.keyboard.isDown(Phaser.Keyboard.UP)){
+            doge.y -= speed;
+            if (doge.y < 500) {
+                doge.y = 500;
+            }
+        }
+
+    }
 };
 
 function changeState(i, stateNum) {
