1009551b30fcc04ba1fd50231b785f5939c964be
meincrab <meincrab@gmail.com>
2018-01-31 23:04:59 +0200

    Tasks 0-7 done, 8 and 9 left to go

--- phaser_eta/state7.js
+++ phaser_eta/state7.js
@@ -1,10 +1,55 @@
+var arrow, startPointX, startPointY, endPointX, endPointY, swipeDirection, leeway = 10;
+
 demo.state7 = function(){};
 demo.state7.prototype = {
-    preload: function() {},
+    preload: function() {
+        game.load.image('arrow', 'assets/sprites/arrow.png');
+    },
     create: function() {
         game.stage.backgroundColor = '#ccff99';
  
         addChangeStateEventListeners();
+
+        arrow = game.add.sprite(centerX, centerY, 'arrow');
+        arrow.anchor.setTo(0.5);
+
+        game.input.onDown.add(this.startSwipe);
+        game.input.onUp.add(this.getSwipeDirection);
+
     },
-    update: function() {}
+    update: function(){}, 
+        startSwipe: function(){
+            console.log(game.input.x + '' + game.input.y);
+            startPointX = game.input.x;
+            startPointY = game.input.y;
+        },
+
+        getSwipeDirection: function() {
+            console.log(game.input.x + '' + game.input.y);
+            endPointX = game.input.x;
+            endPointY = game.input.y;
+
+            if(Math.abs(endPointY - startPointY) < 10 && Math.abs(endPointX - startPointX)) {
+                return false;
+            }
+
+            if(Math.abs(endPointY - startPointY) < Math.abs(endPointX - startPointX)) {
+                console.log('horizontal');
+                if (endPointX > startPointX) {
+                    swipeDirection = 90;
+                }
+                else {
+                    swipeDirection = 270;
+                }
+            } else{
+                console.log('vertical');
+                if (endPointY > startPointY) {
+                    swipeDirection = 180;
+                } else {
+                    swipeDirection = 0;
+                }
+            }
+
+            arrow.angle = swipeDirection;
+        }
 };
